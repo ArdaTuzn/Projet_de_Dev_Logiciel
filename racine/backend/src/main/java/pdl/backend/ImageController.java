9af4f96201a80c4a54915e0d235b90f2b013c1a4
ArdaTuzn <ardtuzn@gmail.com>
2026-02-17 17:20:52 +0100

    ImageDao implemented

--- racine/backend/src/main/java/pdl/backend/ImageController.java
+++ racine/backend/src/main/java/pdl/backend/ImageController.java
@@ -3,10 +3,6 @@ package pdl.backend;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 
-import java.io.IOException;
-import java.util.List;
-
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -32,54 +28,29 @@ public class ImageController {
     this.imageDao = imageDao;
   }
 
-  @RequestMapping(value = "/images/{id}", method = RequestMethod.GET, produces = MediaType.IMAGE_JPEG_VALUE) 
+  @RequestMapping(value = "/images/{id}", method = RequestMethod.GET, produces = MediaType.IMAGE_JPEG_VALUE)
   public ResponseEntity<?> getImage(@PathVariable long id) {
-    return imageDao.retrieve(id)
-        .map(img -> {
-            byte[] bytes = img.getData();
-            return ResponseEntity
-              .ok()
-              .contentType(MediaType.IMAGE_JPEG)
-              .body(bytes);
-          })
-          .orElse(ResponseEntity.notFound().build());
+    // TODO
+    return new ResponseEntity<>(HttpStatus.NOT_FOUND);
   }
 
   @RequestMapping(value = "/images/{id}", method = RequestMethod.DELETE)
   public ResponseEntity<?> deleteImage(@PathVariable long id) {
-    return imageDao.retrieve(id) 
-        .map(img -> {
-            imageDao.delete(img); 
-            return ResponseEntity.noContent().build(); 
-        })
-        .orElse(ResponseEntity.notFound().build()); 
-    }
+    // TODO
+    return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+  }
 
   @RequestMapping(value = "/images", method = RequestMethod.POST)
-  public ResponseEntity<?> addImage(@RequestParam MultipartFile file,
-      RedirectAttributes redirectAttributes) throws IOException {
-        if (!MediaType.IMAGE_JPEG_VALUE.equals(file.getContentType())) {
-          return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).build();
-        }
-        String nameImg = file.getName();
-        byte[] content = file.getBytes();
-        Image img = new Image(nameImg, content);
-        imageDao.create(img);
-    return new ResponseEntity<>(HttpStatus.OK);
+  public ResponseEntity<?> addImage(@RequestParam MultipartFile file) {
+    // TODO
+    return new ResponseEntity<>(HttpStatus.NOT_FOUND);
   }
 
   @RequestMapping(value = "/images", method = RequestMethod.GET, produces = "application/json; charset=UTF-8")
   @ResponseBody
   public ArrayNode getImageList() {
     ArrayNode nodes = mapper.createArrayNode();
-    List<Image> imageList = imageDao.retrieveAll();
-    
-    for (Image img : imageList) {
-        ObjectNode imageNode = mapper.createObjectNode();
-        imageNode.put("id", img.getId());
-        imageNode.put("name", img.getName()); 
-        nodes.add(imageNode);
-    }
+    // TODO
     return nodes;
   }
 
--- racine/backend/src/main/java/pdl/backend/ImageDao.java
+++ racine/backend/src/main/java/pdl/backend/ImageDao.java
@@ -1,6 +1,5 @@
 package pdl.backend;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.ArrayList;
@@ -9,20 +8,13 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-import org.springframework.jdbc.core.RowMapper;
 
 @Repository
-public class ImageDao implements Dao<Image>, InitializingBean {
+public class ImageDao implements Dao<Image> {
 
-  @Autowired
-  private JdbcTemplate jdbcTemplate;
   private final Map<Long, Image> images = new HashMap<>();
-  private Image defaultImage;
 
   public ImageDao() {
     // placez une image test.jpg dans le dossier "src/main/resources" du projet
@@ -31,8 +23,7 @@ public class ImageDao implements Dao<Image>, InitializingBean {
     try {
       fileContent = Files.readAllBytes(imgFile.getFile().toPath());
       Image img = new Image("cat.jpg", fileContent);
-      this.defaultImage = img; 
-      images.put(img.getId(), img); 
+      images.put(img.getId(), img);
     } catch (final IOException e) {
       e.printStackTrace();
     }
@@ -40,26 +31,19 @@ public class ImageDao implements Dao<Image>, InitializingBean {
 
   @Override
   public Optional<Image> retrieve(final long id) {
-    return Optional.ofNullable(images.get(id));
+    Image image = images.get(id);
+    return Optional.ofNullable(image);
   }
 
   @Override
   public List<Image> retrieveAll() {
-    return new ArrayList<Image>(images.values());
-  }  
+    ArrayList<Image> arrayList = new ArrayList<Image>(images.values());
+    return arrayList;
+  }
 
   @Override
   public void create(final Image img) {
-    images.put(img.getId(),img);
-    jdbcTemplate.update("INSERT INTO images (id,name) VALUES (?,?)", img.getId(), img.getName());
-    String folderPath = "src/main/resources/images/";
-    String fileName = img.getName();
-
-    try (FileOutputStream fos = new FileOutputStream(folderPath + fileName)) {
-        fos.write(img.getData()); 
-    } catch (IOException e) {
-        System.err.println("Write Error: " + e.getMessage());
-    }
+    images.put(img.getId(), img);
   }
 
   @Override
@@ -69,14 +53,6 @@ public class ImageDao implements Dao<Image>, InitializingBean {
 
   @Override
   public void delete(final Image img) {
-    images.remove(img.getId());
-  }
-
-  @Override
-  public void afterPropertiesSet() throws Exception {
-    // Drop table
-    jdbcTemplate.execute("DROP TABLE IF EXISTS images");
-    jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS images (id bigserial PRIMARY KEY, name character varying(255))");
-    jdbcTemplate.update("INSERT INTO images (id,name) VALUES (?,?)", defaultImage.getId(), defaultImage.getName());
+    images.remove(img.getId())
   }
 }
